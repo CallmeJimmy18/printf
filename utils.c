8ae8817f230a0b0b1e362fcf5d2a7f0ab458d0ef
Garnet Matola <matolagernet@gmail.com>
2023-04-25 17:12:50 -0700

    update

--- utils.c
+++ utils.c
@@ -23,8 +23,7 @@ int is_printable(char c)
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-	char map_to[] = "0123456789ABCDEF";
-	/* The hexa format code is always 2 digits long */
+char map_to[] = "0123456789ABCDEF";
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
@@ -77,8 +76,11 @@ long int convert_size_number(long int num, int size)
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
-	if (size == S_LONG)
-		return (num);
-	else if (size == S_SHORT)
-		return ((unsigned short)num);
+if (size == S_LONG)
+return (num);
+else if (size == S_SHORT)
+{
+return ((unsigned short)num);
+}
+return ((unsigned int)num);
 }
